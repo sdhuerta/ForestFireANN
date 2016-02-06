ae0ff80990abed832ab2a4547b81fe41b034c7c3
sdhuerta <steven.huerta@mines.sdsmt.edu>
2016-02-06 14:27:35 -0700

    added neuralnetwork stuff

---
+++ neuralnetwork.cpp
@@ -0,0 +1,31 @@
+class neuralnetwork
+{
+public:
+	neuralnetwork(Param net_define);
+
+
+
+private:
+int num_layers;
+float learning;
+float 
+
+
+};
+
+
+
+
+
+
+
+
+
+neuralnetwork::neuralnetwork(Param net_define)
+{
+
+
+
+
+
+}
