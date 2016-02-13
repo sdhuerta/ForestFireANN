032f43423c38b1ac94ca7a575647b7c7f0e5a0ac
sdhuerta <steven.huerta@mines.sdsmt.edu>
2016-02-13 13:21:17 -0700

    working on neural net

--- neuralnetwork.cpp
+++ neuralnetwork.cpp
@@ -4,27 +4,7 @@
 
 using namespace std;
 
- 
-class neuralnetwork
-{
-public:
-	neuralnetwork(Param net_define);
 
-
-
-private:
-vector<int> layers;
-vector<vector<Perceptron>> net ;
-float learning;
-float momentum;                    This header info is now in neuralnetwork.h
-float threshold;
-
-// For training and testing
-vector<float> input ;
-vector<float> output ;
-
-};
-  
 
 
 neuralnetwork::neuralnetwork(Param net_define)
@@ -35,8 +15,9 @@ neuralnetwork::neuralnetwork(Param net_define)
 	momentum = net_define.get_momentumd() ;
 	threshold = net_define.get_threshold();
 
-	// Skip the input layer node
-	for(int i = 1; i < layers.size ; i++)
+	// Skip the input layer node. The layer is n-1, so 
+	// the 1st hidden layer node is layer 0.
+	for(int i = 0; i < layers.size ; i++)
 	{
 		vector<Perceptron> layer ;
 
@@ -54,21 +35,78 @@ neuralnetwork::neuralnetwork(Param net_define)
 }
 
 
-neuralnetwork::training(vector<float> inputs)
+neuralnetwork::training(vector<float> train_inputs, 
+						vector<float> train_ouputs, 
+						int max_iterations, 
+						float min_error)
 {
-	vector<float> outputs
+	input = train_inputs;
+	output = train_outputs;
 
-	for(int i = 0; i < layers.size(); i++)
+	int iterations = 0 ;
+
+	while(iterations++ < max_iterations)
 	{
-		for(int j = 0; j < layers[i].size(); j++)
-		{
-			
+		vector<float> ff_output ;
 
-		}
+		ff_output = feed_foward();
+
+		error = calc_error(ff_output) ;
+
+		if(error < min_error) 
+			break ;
+
+		// If we need to adjust the weights, lets calc the
+		// deltas first
+		calculate_delta(ff_output);
+
+		adjust_weights();
+
+		if(error < min_error) 
+			break ;
+
 	}
 
+}
 
 
+neuralnetwork::feed_foward()
+{
+	// Setup input layer to feed to first hidden layer
+	vector<float> curr_inputs = inputs ;
+
+	// For every layer starting with the hidden layer to the output layer
+	for(int i = 0; i < layers.size(); i++)
+	{
+		vector<float> curr_outputs ;
+		
+		// For every perceptron in this current layer
+		for(int j = 0; j < net[i].size(); j++)
+		{
+			curr_outputs.pushback(net[i][j].get_output(curr_inputs) ;
+		}
+
+		if( i != layers.size() - 1)
+		{
+			vector<float> curr_inputs ;
+			curr_inputs = curr_outputs ;
+		}
+
+	}
+
+	return curr_outputs ;
+
+}
+
+
+neuralnetwork::calculate_delta()
+{
+
+}
+
+
+neuralnetwork::adjust_weights()
+{
 
 }
 
