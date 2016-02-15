3bf84d3a2e4a18b505c056f2e9eef885ce359d03
luke-meyer <luke.meyer@mines.sdsmt.edu>
2016-02-15 16:19:49 -0700

    Implemented saving/loading weights to file in neuralnetwork class

--- neuralnetwork.h
+++ neuralnetwork.h
@@ -3,18 +3,20 @@
 
 
 #include <vector>
-#include <algorithm>
 #include <string>
 #include <fstream>
+#include <iostream>
 #include "perceptron.h"
 
 // TEMPORARY STRUCTURE FOR TESTING ONLY
 // REMOVE BEFORE SUBMISSION!!!
 struct Param{
-	vector<int> layers ;
+	vector<int> layers = { 5, 2, 3, 2 } ; // five inputs, 2 nodes in first h layer, 3 in second h layer, two nodes in o layer
 	float learning_rate ;
 	float momentum ;
 	float min_error;
+        string weightsFile = "FakeWeightsFile.txt";
+   
 } ;
 
 
@@ -28,24 +30,33 @@ class neuralnetwork{
 public:
 	neuralnetwork(Param net_define);
 
-	bool load_weights(string filename);
-	bool save_weights(string filename);
+//	bool load_weights(string filename);
+	//bool save_weights(string filename);
+	void load_weights();
+        void save_weights();
 
-    void training(vector<vector<float>> train_inputs, 
-				  vector<vector<float>> train_ouputs, 
+	void training(vector<float> train_inputs, vector<float> train_ouputs, 
 				  int max_iterations);
 
-	vector<float> testing(vector<float> test_inputs);
+	void testing(vector<float> test_inputs);
+
+        int get_number_o_layers();
+
+        int get_number_o_nodes( int index );
+
+        vector<float> get_perceptron_weights( int layerIndex, int nodeIndex );
 
 
 
 private:
 	vector<int> layers;
 	vector<vector<Perceptron>> net ;
+        int weightLayers;
 
 	float learning;
 	float momentum;
 	float threshold;
+        string weightsFile;
 
 	// For training and testing
 	vector<float> input ;
@@ -54,7 +65,10 @@ private:
 	vector<float> feed_forward();
 	void adjust_weights();
 	float calc_error(vector<float> estimates);
+	//void load_weights();
+        //void save_weights():
 
 };
 
 #endif
+
