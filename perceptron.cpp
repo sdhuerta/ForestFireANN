2efe8903ee200e81290234bca7310d0d40098ae1
sdhuerta <steven.huerta@mines.sdsmt.edu>
2016-02-24 12:29:44 -0700

    comments!

--- perceptron.cpp
+++ perceptron.cpp
@@ -4,14 +4,14 @@
 /******************************************************************************
  * @authors  Steven Huerta, Luke Meyer, Savoy Schuler
  *
- * @par Description:
+ * @par Description: Perceptron contructor. The constructor takes in the 
+ * number of inputs it will receive, as well as the learning rate and 
+ * momentum values. It will generate a vector for weights, initializing
+ * them to a random value. It will also add a bias weight.
  *
- *
- * @param[in]
- *
- * @param[in]
- *
- * @returns
+ * @param[in] num_in - the number of inputs for the perceptron
+ * @param[in] learning_rate - the learning rate factor 
+ * @param[in] mtm - the momentum factor for the perceptron
  *
  *****************************************************************************/
 
@@ -39,14 +39,13 @@ Perceptron::Perceptron(int num_in, float learning_rate, float mtm)
 /******************************************************************************
  * @authors  Steven Huerta, Luke Meyer, Savoy Schuler
  *
- * @par Description:
+ * @par Description: This function will pass the inputs to functions to sum
+ * the product of weights and inputs, it will then call the activation
+ * function to calculate the output and return that output.
  *
+ * @param[in] percept_inputs - a vector of input values
  *
- * @param[in]
- *
- * @param[in]
- *
- * @returns
+ * @returns output - the calculated value after the perceptron fires
  *
  *****************************************************************************/
 
@@ -66,21 +65,16 @@ float Perceptron::calc_output(vector <float> percept_inputs)
 /******************************************************************************
  * @authors  Steven Huerta, Luke Meyer, Savoy Schuler
  *
- * @par Description:
- *
- *
- * @param[in]
- *
- * @param[in]
- *
- * @returns
+ * @par Description: This function sums the product of the weights and inputs.
  *
  *****************************************************************************/
 
 void Perceptron::sum_weights()
 {
+    // reset our sum to zero before calculating our product again
     sum = 0;
 
+    // sum up the products of the weights and inputs
     for(int i = 0; i < num_inputs; i++)
         sum += inputs[i] * weights[i] ;
 
@@ -92,15 +86,8 @@ void Perceptron::sum_weights()
 /******************************************************************************
  * @authors  Steven Huerta, Luke Meyer, Savoy Schuler
  *
- * @par Description:
- *
- *
- * @param[in]
- *
- * @param[in]
- *
- * @returns
- *
+ * @par Description: This function calculates the output by applying the 
+ * sigmoid function to the sum of the products.
  *****************************************************************************/
 
 void Perceptron::activation()
@@ -113,40 +100,42 @@ void Perceptron::activation()
 /******************************************************************************
  * @authors  Steven Huerta, Luke Meyer, Savoy Schuler
  *
- * @par Description:
+ * @par Description: This function calculates the delta for the perceptron 
+ * located in the hidden layer. The function grabs the weights and deltas
+ * from the next layer to calculate the delta for this perceptron.
  *
- *
- * @param[in]
- *
- * @param[in]
- *
- * @returns
- *
+ * @param[in] position - position in the current layer among other perceptrons
+ * @param[in] next_layer - vector of perceptrons from the next layer of the 
+ *                         network.
  *****************************************************************************/
 
-// BACKPROP
-
-// Let's start by calculating the delta
-
-// For our hidden layer weights
 void Perceptron::set_delta(int position, vector<Perceptron> next_layer)
 {
-    delta = 0 ;
-    float next_delta;
+    delta = 0 ; // reset our delta to 0 
+    float next_delta; 
     float next_weight;
 
     // FOR EVERY NODE IN THE FOLLOWING LAYER
     // delta of that layer * connecting weight
 
+    // For all the nodes in the next layer ...
     for(int i = 0; i < next_layer.size(); i++ )
     {
+        // ...let's grab their delta value...
         next_delta = next_layer[i].get_delta();
+
+        //... and let's grab the weight associated between our output
+        // and that next layer's node...
         next_weight = next_layer[i].get_weights()[position] ;
 
+        //.. and then accumulate their product
         delta += next_delta * next_weight   ;
     }
 
 
+    // this is the product of the delta and sigmoid prime with
+    // an extra multiplier to prevent super small increments
+    // This is a WAG factor
     delta = 2.0 * delta * output * (1 - output) ;
 }
 
@@ -154,10 +143,10 @@ void Perceptron::set_delta(int position, vector<Perceptron> next_layer)
 /******************************************************************************
  * @authors  Steven Huerta, Luke Meyer, Savoy Schuler
  *
- * @par Description:
+ * @par Description: This function calculates the delta values for output 
+ * layer nodes. 
  *
- *
- * @param[in]
+ * @param[in]   
  *
  * @param[in]
  *
@@ -165,13 +154,9 @@ void Perceptron::set_delta(int position, vector<Perceptron> next_layer)
  *
  *****************************************************************************/
 
-// Four our output layer weights
 void Perceptron::set_delta(float correct_output)
 {
     delta = -2.0 * (correct_output - output) * output * (1.0 - output);
-    //REMOVE BEFORE SUBMISSION!!!!!
-    // printf("OUTPUT LAYER DELTA: %6.4f TARGET: %6.4f OUTPUT: %6.4f\n",delta,
-    //        correct_output, output);
 }
 
 
@@ -199,33 +184,30 @@ float Perceptron::get_delta()
 /******************************************************************************
  * @authors  Steven Huerta, Luke Meyer, Savoy Schuler
  *
- * @par Description:
- *
- *
- * @param[in]
- *
- * @param[in]
- *
- * @returns
+ * @par Description: This function makes a change to each of the weights 
+ * for the perceptron. 
  *
  *****************************************************************************/
 
-// Let's adjust weights for all layers
 void Perceptron::adjust_weights()
 {
+    // To store our calculated momentum
     float temp_momentum;
 
+    // For each of our weights...
     for(int i = 0 ; i < weights.size(); i++)
     {
+        //...let's calculate the momentum...
         temp_momentum =  momentum * (weights[i] - old_weights[i]);
-
+        //...and unless its the bias...
         if( i < weights.size()-1)
         {
-            weights[i] = weights[i] - alpha * delta * inputs[i];
-            // + temp_momentum;
+            //...we'll subtract a fraction of the product of the delta and
+            // and the input, then add some back(momentum)...
+            weights[i] = weights[i] - alpha * delta * inputs[i] + temp_momentum;
         }
         else
-            weights[i] = weights[i] - alpha * delta;// + temp_momentum;
+            weights[i] = weights[i] - alpha * delta + temp_momentum;
     }
 
     old_weights = weights ;
@@ -236,14 +218,10 @@ void Perceptron::adjust_weights()
 /******************************************************************************
  * @authors  Steven Huerta, Luke Meyer, Savoy Schuler
  *
- * @par Description:
+ * @par Description: This function sets its weights to the vector of weights
+ * received.
  *
- *
- * @param[in]
- *
- * @param[in]
- *
- * @returns
+ * @param[in] new_weights vector of the weights
  *
  *****************************************************************************/
 
@@ -256,14 +234,10 @@ void Perceptron::set_weights(vector<float> new_weights)
 /******************************************************************************
  * @authors  Steven Huerta, Luke Meyer, Savoy Schuler
  *
- * @par Description:
+ * @par Description: This function returns a vector of weights for this
+ * perceptron.
  *
- *
- * @param[in]
- *
- * @param[in]
- *
- * @returns
+ * @returns weights - vector of weights
  *
  *****************************************************************************/
 
